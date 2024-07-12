e036055a9022c5d58a762b4f303b9ee44b6e4052
anhkiethr300 <anhkiethr300@gmail.com>
2024-07-12 21:33:05 +0700

    lesson 3+4

--- Lesson-3+4/classes/ball.py
+++ Lesson-3+4/classes/ball.py
@@ -2,10 +2,11 @@ import pygame
 
 
 class Ball:
-    def __init__(self, img_url, speed_x, speed_y) -> None:
+    def __init__(self, img_url, speed_x, speed_y, lose) -> None:
         self.speed_x = speed_x
         self.speed_y = speed_y
         self.img_url = pygame.image.load(img_url)
+        self.lose = lose
 
     # set location
     def setX(self, x):
@@ -39,13 +40,13 @@ class Ball:
         ):
             self.speed_x *= -1
         
-        if self.x <= (paddle_2_x + paddle_2_distance) and paddle_2_y <= self.y <= (
+        if self.x >= (paddle_2_x + paddle_2_distance) and paddle_2_y <= self.y <= (
             paddle_2_y + 120
         ):
             self.speed_x *= -1
 
         # kiem tra cham edge
-        if self.x <= 0 or self.x >= 570:
-            self.speed_x = -self.speed_x
-        if self.y <= 0 or self.y >= 570:
-            self.speed_y = -self.speed_y
+        if self.x <= 0 or self.x >= 580:
+            self.lose = True
+        if self.y <= 0 or self.y >= 580:
+            self.speed_y *= -1
--- Lesson-3+4/pingponggame.py
+++ Lesson-3+4/pingponggame.py
@@ -10,14 +10,16 @@ class Main:
         title,
         size,
         bg,
-        end_bg,
+        win_bg,
+        lose_bg,
         ball_img_link,
         paddle_img_link,
         ball_speed,
         ball_location,
         paddle_1_location,
         paddle_2_location,
-        time
+        time,
+        end
     ) -> None:
         # setup canvas
         pygame.init()
@@ -25,12 +27,13 @@ class Main:
         self.canvas = pygame.display.set_mode(size)
         self.clock = pygame.time.Clock()
         self.bg = bg
-        self.end_bg = end_bg
+        self.win_bg = win_bg
+        self.lose_bg = lose_bg
         self.ball_img_link = ball_img_link
         self.paddle_img_link = paddle_img_link
         # create objects (player, ball, paddles)
         self.ball = Ball(
-            img_url=ball_img_link, speed_x=ball_speed[0], speed_y=ball_speed[1]
+            img_url=ball_img_link, speed_x=ball_speed[0], speed_y=ball_speed[1], lose=False
         )
         self.paddle_1 = Paddle(img_url=paddle_img_link)
         self.paddle_2 = Paddle(img_url=paddle_img_link)
@@ -42,8 +45,8 @@ class Main:
         self.paddle_1.setY(y=paddle_1_location[1])
         self.paddle_2.setX(x=paddle_2_location[0])
         self.paddle_2.setY(y=paddle_2_location[1])
-
         self.time = time
+        self.end = end
 
     def run(self):
         while True:  # play game
@@ -76,11 +79,12 @@ class Main:
             self.ball.move(
                 paddle_1_x=self.paddle_1.getX(),
                 paddle_1_y=self.paddle_1.getY(),
-                paddle_1_distance=10,
+                paddle_1_distance= 25,
                 paddle_2_x=self.paddle_2.getX(),
                 paddle_2_y=self.paddle_2.getY(),
-                paddle_2_distance=-10,
+                paddle_2_distance= -18,
             )
+        
 
             # kiem tra nut bam de di chuyen paddles
             if self.player.getKeyUp1():
@@ -91,41 +95,66 @@ class Main:
                 self.paddle_2.move(-1, 0, 480)
             if self.player.getKeyDown2():
                 self.paddle_2.move(1, 0, 480)
+            
+            
+            
+            if self.end == False:
+                # show canvas
+                self.canvas.fill(self.bg)
+                self.canvas.blit(self.ball.img_url, (self.ball.getX(), self.ball.getY()))
+                self.canvas.blit(
+                    self.paddle_1.img, (self.paddle_1.getX(), self.paddle_1.getY())
+                )
+                self.canvas.blit(
+                    self.paddle_2.img, (self.paddle_2.getX(), self.paddle_2.getY())
+                )
+                self.check_lose()
+                self.show_time()
+                self.time_counter()
+                self.clock.tick(60)
+            else:
+                self.clock.tick(0)
 
-            # show canvas
-            self.canvas.fill(self.bg)
-            self.canvas.blit(self.ball.img_url, (self.ball.getX(), self.ball.getY()))
-            self.canvas.blit(
-                self.paddle_1.img, (self.paddle_1.getX(), self.paddle_1.getY())
-            )
-            self.canvas.blit(
-                self.paddle_2.img, (self.paddle_2.getX(), self.paddle_2.getY())
-            )
-            self.clock.tick(60)
 
-            self.time_counter()
-            self.show_time()
-
+            # update
             pygame.display.flip()
 
     def time_counter(self):
         self.time -= 1/60
         if self.time < 25:
-            self.end()
+            self.end = True
+            self.win()
 
     
     def show_time(self):
-        if self.time > 25:
+        if self.time >= 0 and self.ball.lose == False:
             if pygame.font:
                 font = pygame.font.Font(None, 36)
                 time = font.render(f"time left: {self.time:.0f}", 1, (255, 255, 255))
-                self.canvas.blit(time, (0, 0))
-    def end(self):
-        self.canvas.fill(self.end_bg)
+                self.canvas.blit(time, (2, 2))
+
+    def check_lose(self):
+        if self.ball.lose == True:
+            self.end = True
+            self.lose()
+
+    def win(self):
+        self.canvas.fill(self.win_bg)
         if pygame.font:
             font = pygame.font.Font(None, 36)
-            text = font.render("END", 1, (255, 255, 255))
-            self.canvas.blit(text, (275, 280))
+            text = font.render("You win!", 1, (255, 255, 255))
+            self.canvas.blit(text, (260, 280))
+
+    def lose(self):
+        self.canvas.fill(self.lose_bg)
+        if pygame.font:
+            font = pygame.font.Font(None, 36)
+            text = font.render("HAHAHAAHA", 1, (255, 255, 255))
+            self.canvas.blit(text, (230, 250))
+            text = font.render("You lose!", 1, (255, 255, 255))
+            self.canvas.blit(text, (260, 280))
+
+        
 
 
 # Driver code
@@ -133,13 +162,15 @@ main = Main(
     title="Ping Pong",
     size=(600, 600),
     bg=(252, 16, 140),
-    end_bg = (158, 44, 171),
+    win_bg = (223, 245, 22),
+    lose_bg = (59, 34, 97),
     ball_img_link="Lesson-3+4\\assets\\ball.png",
     paddle_img_link="Lesson-3+4\\assets\\paddle.png",
     ball_location=(285, 300),
-    ball_speed=(1.5, 3.5),
+    ball_speed=(3, 2.5),
     paddle_1_location=(0, 250),
     paddle_2_location=(570, 250),
-    time = 30
+    time = 30,
+    end = False
 )
 main.run()
