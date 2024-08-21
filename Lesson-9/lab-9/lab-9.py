29b5f66e1da003e22ad6a46d3a0d153c63c9c5e4
anhkiethr300 <anhkiethr300@gmail.com>
2024-08-21 17:54:40 +0700

    lab9

--- Lesson-9/lab-9/lab-9.py
+++ Lesson-9/lab-9/lab-9.py
@@ -2,24 +2,21 @@ from data import col_list, province_list
 import pandas as pd
 
 
-# series --------------------------------
-# 1 cot
-# series = pd.Series(province_list, index=['a', 'b', 'c'])
-# data = {'a': 'abc', 'b': 'xyz', 'c': 'mno'}
-
-# series = pd.Series(data=data, index=['a', 'b', 'c'])
-
-# data = {"a": -1.3, "b": 11.7, "d": 2.0, "f": 10, "g": 5}
-# series = pd.Series(data, index=["a", "b", "c", "d", "e", "f"])
-# print(series[0:6:2])
-
-
-
-
 # dataFrame -----------------------------
 data = pd.DataFrame(data=province_list, columns=col_list)
-# print(data.head(10))
 
 
-# luu vao xls
-data.to_excel("Lesson-9/lab-9/province.xlsx")
+num_rows, num_cols = data.shape
+print(f"Số dòng: {num_rows}")
+print(f"Số cột: {num_cols}")
+
+
+memory_usage = data.memory_usage()
+print(f"Kích thước lưu trữ của df trong bộ nhớ: {memory_usage.sum()} bytes")
+
+
+print(f"Kiểu dữ liệu của từng cột trong df:\n{data.dtypes}")
+
+
+# luu vao xlsx
+# data.to_excel("Lesson-9/lab-9/province.xlsx")
